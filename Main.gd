2651c6ae89b6f7269ff26aa85adf5f007101c0c1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-29 12:03:32 +0900

    (Main.gd) Playerクラスの利用に移行

--- Main.gd
+++ Main.gd
@@ -1,29 +1,7 @@
 extends Spatial
 
-var rank = 0
-var point = 0
-var year = 0
-var age = 25
-var number_of_papers = 0
-var researcher_level = 0
-var hour = 600
-var writing_hour = 0
-var number_of_students = 0
-var number_of_postdocs = 0
-var university = "T大"
-var level_in_university = 1
-var university_point = 10
-var level_in_society = 1
-var connection_point = 10
-var contribution_point = 10
+var player = load("model/Player.gd").new()
 var step = 0
-var event_hour = 0
-var money = 500
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -50,18 +28,15 @@ func _on_Button_pressed():
 	$Console/Level2Button.disabled = true
 	$Console/Level3Button.disabled = true
 	if 2 <= step:
-		writing_hour += hour
-		step = 0
-		hour = 600
-		event_hour = 0
-		if 950 < writing_hour:
+		player.turn_end()
+		if 950 < player.writing_hour:
 			$Console/Level1Button.disabled = false
-		if 1500 < writing_hour:
+		if 1500 < player.writing_hour:
 			$Console/Level2Button.disabled = false
-		if 2200 < writing_hour:
+		if 2200 < player.writing_hour:
 			$Console/Level3Button.disabled = false
 	var new_year = false
-	if rank == 0:
+	if player.rank == 0:
 		new_year = $GameSpace/AssistantLife.go_forward(s)
 	else:
 		new_year = $GameSpace/ProfessorLife.go_forward(s)
@@ -73,59 +48,49 @@ func _on_event_happened(priority, message):
 	# return
 	$EventHappened.display(message["id"])
 	if message["type"] == "univ":
-		event_hour += message["hour"]
+		player.event_hour += message["hour"]
+		player.event_hour += message["hour"]
 		update_research_hour()
-	point += priority
 
 func year_end():
-	year += 1
-	if 30 < year:
-		$StatuReport.display("十分な実績を残すことができませんでした")
-		# FIXME 強制終了の処理
-		return
-	if 10 < point and rank == 0:
-		rank = 1
-		point = 0
-		$GameSpace/AssistantLife.hide()
-		for e in $GameSpace/ProfessorLife.period:
-			e.upgrade()
-		$GameSpace/ProfessorLife.show()
-		$StatusReport.display("教授になりました")
-	else:
-		$StatusReport.display("研究者になって%s年が経ちました" %  year)
-	number_of_postdocs = 0
-	number_of_students = 0
-	money = 500
+	var ret = player.year_end()
+	match ret["kind"]:
+		0:
+			$StatusReport.display("研究者になって%s年が経ちました" %  player.year)
+		1:
+			$StatuReport.display(ret["message"])
+			# FIXME 強制終了の処理
+			return
+		2:
+			$StatusReport.display(ret["message"])
+			$GameSpace/AssistantLife.hide()
+			for e in $GameSpace/ProfessorLife.period:
+				e.upgrade()
+			$GameSpace/ProfessorLife.show()
 
 func update_research_hour():
-	hour += number_of_students * 50
-	$ResearchPanel/Panel/Resource/TimeTable/Team/Student.text = "%s(%s人)" % [number_of_students, number_of_students * 50]
-	hour += number_of_postdocs * 50
-	$ResearchPanel/Panel/Resource/TimeTable/Team/PostDoc.text = "%s(%s人)" % [number_of_postdocs, number_of_postdocs * 50]
-	$ResearchPanel/Panel/Resource/TimeTable/Personal/Time.text = "%s" % (hour - event_hour)
-	hour -= level_in_university * 40
-	$ResearchPanel/Panel/Resource/TimeTable/Personal/JobForUniv.text = "%s" % (level_in_university * -40)
-	hour -= level_in_society * 40
-	$ResearchPanel/Panel/Resource/TimeTable/Personal/JobForSoc.text = "%s" % (level_in_society * -40)
-	if hour < 0:
-		writing_hour += hour
-		hour = 0
-	$ResearchPanel/Panel/Resource/TimeTable/Personal/TotalHour.text = "%s" % (hour - event_hour)
-	$ResearchPanel/Panel/Resource/TimeTable/Personal/WritingTime.text = "%s" % writing_hour
+	player.update_research_hour()
+	$ResearchPanel/Panel/Resource/TimeTable/Team/Student.text = "%s(%s人)" % [player.number_of_students, player.number_of_students * 50]
+	$ResearchPanel/Panel/Resource/TimeTable/Team/PostDoc.text = "%s(%s人)" % [player.number_of_postdocs, player.number_of_postdocs * 50]
+	$ResearchPanel/Panel/Resource/TimeTable/Personal/Time.text = "%s" % (player.hour - player.event_hour)
+	$ResearchPanel/Panel/Resource/TimeTable/Personal/JobForUniv.text = "%s" % (player.level_in_university * -40)
+	$ResearchPanel/Panel/Resource/TimeTable/Personal/JobForSoc.text = "%s" % (player.level_in_society * -40)
+	$ResearchPanel/Panel/Resource/TimeTable/Personal/TotalHour.text = "%s" % (player.hour - player.event_hour)
+	$ResearchPanel/Panel/Resource/TimeTable/Personal/WritingTime.text = "%s" % player.writing_hour
 	$RsearchPanel/Panel/Resource/Resource
 
 func update_state_panel():
-	$StatusPanel/Panel/Status/Personal/ResearchLevel.text = '%dx%d' % [researcher_level, 0]
-	$StatusPanel/Panel/Status/Personal/UniversityPoint.text = '%d' % university_point
-	$StatusPanel/Panel/Status/Personal/ConnectionPoint.text = '%d' % connection_point
-	$StatusPanel/Panel/Status/Personal/ContributionPoint.text = '%d' % contribution_point
-	$StatusPanel/Panel/Status/Personal/Money.text = '%d' % money
+	$StatusPanel/Panel/Status/Personal/ResearchLevel.text = '%dx%d' % [player.skill_level, 0]
+	$StatusPanel/Panel/Status/Personal/UniversityPoint.text = '%d' % player.university_point
+	$StatusPanel/Panel/Status/Personal/ConnectionPoint.text = '%d' % player.connection_point
+	$StatusPanel/Panel/Status/Personal/ContributionPoint.text = '%d' % player.contribution_point
+	$StatusPanel/Panel/Status/Personal/Money.text = '%d' % player.money
 
 func _on_Level1Button_pressed():
 	$Console/Level1Button.disabled = true
 	$Console/Level2Button.disabled = true
 	$Console/Level3Button.disabled = true
-	writing_hour = 0
+	player.writing_hour = 0
 	update_research_hour()
 	$StatusReport.display("論文を投稿しました")
 
@@ -133,7 +98,7 @@ func _on_Level2Button_pressed():
 	$Console/Level1Button.disabled = true
 	$Console/Level2Button.disabled = true
 	$Console/Level3Button.disabled = true
-	writing_hour = 0
+	player.writing_hour = 0
 	update_research_hour()
 	$StatusReport.display("論文を投稿しました")
 
@@ -141,6 +106,6 @@ func _on_Level3Button_pressed():
 	$Console/Level1Button.disabled = true
 	$Console/Level2Button.disabled = true
 	$Console/Level3Button.disabled = true
-	writing_hour = 0
+	player.writing_hour = 0
 	update_research_hour()
 	$StatusReport.display("論文を投稿しました")
